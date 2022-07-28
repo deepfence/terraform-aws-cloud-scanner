f8ede697f2b05bbcadc7e6afe7dfece6be8bd7f1
Nitin Naidu <naidunitin1@gmail.com>
2022-07-28 21:53:39 +0530

    Refactor (#1)
    * refactor: dir structure refactor
    
    * chore: link correction for readme

--- examples/organizational-deploy-with-member-account-read-write-access-creation/main.tf
+++ examples/organizational-deploy-with-member-account-read-write-access-creation/main.tf
@@ -1,10 +1,16 @@
 # module creates resource group
 
 module "resource_group" {
+  source = "../../modules/infrastructure/resource-group"
+  name   = var.name
+  tags   = var.tags
+}
+
+module "resource_group_secure_for_cloud_member" {
   providers = {
     aws = aws.member
   }
-  source = "../../../modules/infrastructure/resource-group"
+  source = "../../modules/infrastructure/resource-group"
   name   = var.name
   tags   = var.tags
 }
@@ -16,7 +22,7 @@ module "vpc-ecs" {
     aws = aws.member
   }
 
-  source             = "../../../modules/infrastructure/vpc-ecs"
+  source             = "../../modules/infrastructure/vpc-ecs"
   ecs_vpc_region_azs = var.ecs_vpc_region_azs
   name               = var.name
   tags               = var.tags
@@ -30,12 +36,12 @@ module "ecs-service" {
   }
   is_organizational = true
   organizational_config = {
-    mgmt_acc_role_arn               = aws_iam_role.ccs_ecs_task_role.arn
+    mgmt_acc_role_arn               = module.org-role-ecs.ccs_mgmt_acc_role_arn
     organizational_role_per_account = var.organizational_member_default_admin_role
     mem_acc_ecs_task_role_name      = aws_iam_role.ccs_ecs_task_role.name
   }
 
-  source                      = "../../../modules/services/ecs-service"
+  source                      = "../../modules/services/ecs-service"
   aws-region                  = var.region
   ecs_vpc_subnets_private_ids = module.vpc-ecs.ecs_vpc_subnets_private_ids
   ecs_cluster_name            = "${var.name}-ecs-cluster"
