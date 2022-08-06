341a7626798373c386254a8906bcb3dce10d1f87
Ramanan Ravikumar <ramanan@deepfence.io>
2022-08-06 13:48:10 +0530

    Add shebang line

--- examples/organizational-deploy-with-member-account-read-only-access-creation/startup.sh
+++ examples/organizational-deploy-with-member-account-read-only-access-creation/startup.sh
@@ -1,8 +1,9 @@
-# this bash script will download the required jinja template files from Deepfence repository, 
+#!/bin/bash
+
+# this bash script will download the required jinja template files from Deepfence repository,
 # delete any non required files/dir for user, install dependencies to run jinja2 template, 
 # run jinja template to create terraform files,
 # run the terraform files to create the required access in member accounts
-#!/bin/bash
 git clone https://github.com/deepfence/terraform-aws-cloud-scanner.git
 cd terraform-aws-cloud-scanner/examples/organizational-deploy-with-member-account-read-only-access-creation/member-account-access-creation-files/
 mv * ../../../../
